c6bb0cd36fd5e7a6ba69d842b60bcd1c8e05c021
Nico Ismaili <nico@ismaili.de>
2022-03-26 22:00:37 +0100

    ✨ Added brightness detection to decide if watermark needs to be inverted

--- watermarker.py
+++ watermarker.py
@@ -1,40 +1,31 @@
 import os
 import string
-import math
-from email.utils import parseaddr
-
+import numpy as np
 from PIL import Image
 
 
-def watermark(img_path: string, output_path: string, watermark_path: string = './watermark/sample.png', rel_size: float = 0.021, padding: tuple[int, int] = (200, 200), pos: string = 'BL', opacity: float = 0.6):
+def watermark(img_path: string, output_path: string, watermark_path: string = './watermark/sample.png', rel_size: float = 0.021, padding: tuple[int, int] | tuple[float, float] = (200, 200), pos: string = 'BL', opacity: float = 0.6):
     assert os.path.splitext(os.path.basename(watermark_path))[
         1] == '.png', "Watermark file must be of type PNG."
-    assert pos in ['TL', 'TR', 'BL', 'BR'], "Specified watermark position is invalid. Valid position values are TL, TR, BL and BR"
+    assert pos in ['TL', 'TR', 'BL',
+                   'BR'], "Specified watermark position is invalid. Valid position values are TL, TR, BL and BR"
+    # Get base image
     base_image = Image.open(img_path)
     img_w, img_h = base_image.size
 
+    # Get watermark
     wm_img = Image.open(watermark_path)
     wm_img.convert("RGBA")
     wm_w, wm_h = wm_img.size
     img_area = img_w * img_h
     wm_ratio = wm_w / wm_h
-    # Calculate new watermark size based on relative size
-    new_wm_h = math.sqrt(rel_size * img_area / wm_ratio)
-    new_wm_w = wm_ratio * new_wm_h
-    # Resize watermark
-    wm_img = wm_img.resize((int(new_wm_w), int(new_wm_h)))
-    # Adjust opacity
-    wm_color_data = wm_img.getdata()
-    new_color_data = []
-    alpha = int(255*opacity)
-    for pixel in wm_color_data:
-        if pixel[3] != 0:
-            new_color_data.append((255, 255, 255, alpha))
-        else:
-            new_color_data.append(pixel)
 
-    wm_img.putdata(new_color_data)
-    # Find positional data for watermark based on given position value
+    # Resize watermark based on relative size
+    new_wm_h = np.sqrt(rel_size * img_area / wm_ratio)
+    new_wm_w = wm_ratio * new_wm_h
+    wm_img = wm_img.resize((int(new_wm_w), int(new_wm_h)))
+
+    # Find positional data for watermark based on given pos value
     wm_w, wm_h = wm_img.size
     wm_pos = ()
     if pos == 'TL':
@@ -45,6 +36,31 @@ def watermark(img_path: string, output_path: string, watermark_path: string = '.
         wm_pos = (img_w - padding[0] - wm_w, img_h - padding[1] - wm_h)
     elif pos == 'BL':
         wm_pos = (padding[0], img_h - padding[1] - wm_h)
+
+    # Decide if watermark needs to be inverted based on watermark background area
+    base_is_dark = True
+    # The crop method from the Image module takes four coordinates as input.
+    # The right can also be represented as (left+width)
+    # and lower can be represented as (upper+height).
+    wm_gs_area = base_image.copy().convert('L').crop(
+        (wm_pos[0], wm_pos[1], wm_pos[0] + wm_w, wm_pos[1]+wm_h))
+    img_data = np.asarray(wm_gs_area.getdata())
+    if np.mean(img_data) > 150:
+        base_is_dark = False
+
+    # Adjust opacity (and invert)
+    wm_color_data = wm_img.getdata()
+    new_color_data = []
+    alpha = int(255*opacity)
+    for pixel in wm_color_data:
+        if pixel[3] != 0:
+            if base_is_dark:
+                new_color_data.append((255, 255, 255, alpha))
+            else:
+                new_color_data.append((0, 0, 0, alpha))
+        else:
+            new_color_data.append(pixel)
+    wm_img.putdata(new_color_data)
 
     output_img = Image.new('RGBA', (img_w, img_h), (0, 0, 0, 0))
     output_img.paste(base_image, (0, 0))
