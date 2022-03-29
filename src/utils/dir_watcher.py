47e65a8d40853343d2a616baf17b974e9ccfe0ab
Nico Ismaili <nico@ismaili.de>
2022-03-29 21:10:47 +0200

    📝 Add documentation to dir_watcher

--- src/utils/dir_watcher.py
+++ src/utils/dir_watcher.py
@@ -1,12 +1,39 @@
 import string
 import time
 
+
 from watchdog.observers import Observer
 
 from utils.image_event_handler import ImageEventHandler
 
 
 class DirWatcher:
+    """
+    Directory watcher implementation based on the watchdog.observers' Observer module.
+
+    ...
+
+    Attributes
+    ----------
+    input_path : str
+        path to the to-be-observed directory
+    event_handler : utils.image_event_handler
+        object which is called upon when events are fired
+    event_observer : Observer
+        watchdog object which handles directory supervision
+
+    Methods
+    -------
+    run:
+        starts infinite watch loop
+    start:
+        euns __schedule and starts the Observer
+    stop:
+        terminates the Observer
+    __schedule:
+        initializes Observer
+    """
+
     def __init__(self, input_path: string, output_path: string, watermark_path: string, rel_size: float, padding: tuple[int, int] or float, pos: string, opacity: float):
         self.__input_path = input_path
         self.__event_handler = ImageEventHandler(
