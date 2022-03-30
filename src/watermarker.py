8b9951fca08468178ea3b3a242790bda744e91f6
Nico Ismaili <nico@ismaili.de>
2022-03-30 14:28:21 +0200

    🐛 Fixed relative path acceptance in config

--- src/watermarker.py
+++ src/watermarker.py
@@ -1,13 +1,12 @@
 """
 Main module responsible for initial configuration and path finding. 
 """
-from concurrent.futures import thread
-import os
 import json
+import os
 
 from utils.dir_watcher import DirWatcher
-from utils.error_handling import check_bool, check_color, check_padding, check_path, check_percentage, check_pos, check_rec_watch
-from utils.watermark import watermark
+from utils.error_handling import (check_bool, check_color, check_padding,
+                                  check_path, check_percentage, check_pos)
 
 if __name__ == '__main__':
     # Load config from JSON
@@ -23,16 +22,16 @@ if __name__ == '__main__':
                     'watermark_path': os.path.join(resource_base_path, 'watermark/sample.png'), 'rel_size': 0.03, 'padding': 0.6, 'pos': 'BL', 'opacity': 0.7, 'threshold': 150, 'rec_watch': False}
 
     # Get parameter values from config or use default values if not defined or out of bounds
-    input_path = config.get(
-        'unmarkedDirPath', default_vals['input_path'])
+    input_path = os.path.abspath(config.get(
+        'unmarkedDirPath', default_vals['input_path']))
     if check_path(input_path, 'unmarked directory'):
         input_path = default_vals['input_path']
-    output_path = config.get(
-        'markedDirPath', default_vals['output_path'])
+    output_path = os.path.abspath(config.get(
+        'markedDirPath', default_vals['output_path']))
     if check_path(output_path, 'marked directory'):
         output_path = default_vals['output_path']
-    watermark_path = config.get(
-        'watermarkPath', default_vals['watermark_path'])
+    watermark_path = os.path.abspath(config.get(
+        'watermarkPath', default_vals['watermark_path']))
     if check_path(watermark_path, 'watermark file'):
         watermark_path = default_vals['watermark_path']
     pos = config.get('pos', default_vals['pos'])
@@ -50,13 +49,9 @@ if __name__ == '__main__':
     threshold = config.get('threshold', 150)
     if check_color(threshold, 'threshold'):
         threshold = default_vals['threshold']
-    # Get padding and convert to tuple if necessary
     padding = config.get('padding', 0.6)
     if check_padding(padding):
         padding = default_vals['padding']
-    else:
-        if check_percentage(padding, 'padding'):
-            padding = default_vals['padding']
     # Open unmarked folder
     os.startfile(input_path)
     # Start watching folder
